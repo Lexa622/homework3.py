2bfd4a7b38a721644d72ba70341ffc6b47a60304
Lexa622 <lexa622@gmail.com>
2024-05-22 22:00:58 +0200

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,4 +1,7 @@
 my_string = input('введите произвольный текст: ')
-print('длинна символов ', len(my_string), '\nстрока в верхнем регистре ', my_string.upper(),
-      '\nстрока в нижнем регистре ', my_string.lower(), '\nстрока без пробелов ', my_string.replace(' ', ''),
-      '\nпервый символ строки ', my_string[0], '\nпоследний символ строки', my_string[len(my_string) - 1])
+print('длинна символов: ', len(my_string))
+print('строка в верхнем регистре: ', my_string.upper())
+print('строка в нижнем регистре: ', my_string.lower())
+my_string.replace(' ', '')
+print('первый символ строки: ', my_string[0])
+print('последний символ строки:', my_string[len(my_string) - 1])
